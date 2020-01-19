2ef43a93b401e43de1725137c4bf449f9cfee7b7
snailvoyager <kyj88514@gmail.com>
2020-01-19 20:25:38 +0900

    DoublyLinkedList add(), remove() 수정

--- SW Certi/src/DataStructures/DoublyLinkedList/DoublyLinkedList.java
+++ SW Certi/src/DataStructures/DoublyLinkedList/DoublyLinkedList.java
@@ -18,51 +18,126 @@ public class DoublyLinkedList {
 	}
 	
 	public boolean addFirst(Object input) {
-		return true;
-	}
-	
-	public boolean addLast(Object input) {
 		Node newNode = new Node(input);
 		
-		if(head == null) {
-			head = newNode;
-		} else {
-			tail.next = newNode;	//새 노드의 왼쪽 연결
-			newNode.prev = tail;	//새 노드의 오른쪽 연결
+		if(head != null) {			//기존 노드가 있으면 순서변경
+			newNode.next = head;
+			head.prev = newNode;
 		}
-		tail = newNode;
+		head = newNode;
+		
+		if(head.next == null) {		//다음 노드가 없으면 tail 값 동일
+			tail = head;
+		}
 		
 		size++;
 		return true;
 	}
 	
-	public Object get(int index) {
-		Node cur = head;
-		
-		for(int i=0; i<index; i++) {
-			cur = cur.next;
+	public boolean addLast(Object input) {
+		if(size == 0) {				//첫 데이터라면 head에 넣기
+			return addFirst(input);
+		} else {
+			Node newNode = new Node(input);
+			
+			tail.next = newNode;	//새 노드의 왼쪽 연결
+			newNode.prev = tail;	//새 노드의 오른쪽 연결
+			tail = newNode;
+			
+			size++;
+			return true;
+		}
+	}
+	
+	public boolean add(int index, Object input) {
+		if(index == 0) {
+			addFirst(input);
+		} else if(index == size-1) {
+			addLast(input);
+		} else {
+			Node newNode = new Node(input);
+			Node before = getNode(index - 1);
+			Node next = before.next;
+			
+			before.next = newNode;	//새 노드 왼쪽 연결
+			newNode.prev = before;
+			
+			next.prev = newNode;	//새 노드 오른쪽 연결
+			newNode.next = next;
+			
+			size++;
 		}
 		
+		return true;
+	}
+	
+	public Node getNode(int index) {
+		Node node = null;
+		
+		if(index < size / 2){	//head 부터 탐색
+			node = head;
+			for(int i=0; i<index; i++) {
+				node = node.next;
+			}
+		} else {				//tail 부터 탐색
+			node = tail;
+			for(int i=size-1; i>index; i--) {
+				node = node.prev;
+			}
+		}
+		return node;
+	}
+	
+	public Object get(int index) {
+		Node cur = getNode(index);
 		return cur.data;
 	}
 	
-	public Object remove(int index) {
-		Node beforeNode = head;
+	public Object removeFirst() {
 		Node delNode = head;
-		
-		for(int i=0; i<index; i++) {
-			beforeNode = delNode;
-			delNode = delNode.next;
-		}
-		beforeNode.next = delNode.next;
-		delNode.next.prev = beforeNode;
-		
 		Object returnData = delNode.data;
 		
+		head.next.prev = null;
+		head = head.next;		//head 다음 노드로 변경
 		delNode = null;
 		
 		size--;
-		return returnData;
+		return returnData; 
+	}
+	
+	public Object removeLast() {
+		Node delNode = tail;
+		Object returnData = delNode.data;
+		
+		tail.prev.next = null;	//tail 이전 노드로 변경
+		tail = tail.prev;
+		delNode = null;
+		
+		size--;
+		return returnData; 
+	}
+	
+	public Object remove(int index) {
+		if(size == 0) {
+			return null;
+		} else if(index == 0){
+			return removeFirst();
+		} else if(index == size-1){
+			return removeLast();
+		} else {
+			Node delNode = getNode(index);
+			Node beforeNode = delNode.prev;
+			
+			beforeNode.next = delNode.next;		//이전 노드 연결
+			delNode.next.prev = beforeNode;		//다음 노드 연결
+			
+			Object returnData = delNode.data;
+			
+			delNode = null;
+			
+			size--;
+			return returnData;
+		}
 	}
 	
 	public String toString() {
--- SW Certi/src/DataStructures/DoublyLinkedList/Main.java
+++ SW Certi/src/DataStructures/DoublyLinkedList/Main.java
@@ -11,12 +11,15 @@ public class Main {
 		dList.addLast(33);
 		dList.addLast(44);
 		dList.addLast(55);
+		dList.add(3, 66);
 		
 		System.out.println("List size : " + dList.size());
 		System.out.println(dList.toString());
 		
-		System.out.println("List get : " + dList.get(2));
-		System.out.println("List remove : " + dList.remove(2));
+		System.out.println("List get : " + dList.get(3));
+		System.out.println("List remove : " + dList.remove(3));
+		dList.remove(0);
+		dList.removeLast();
 		
 		System.out.println("List size : " + dList.size());
 		System.out.println(dList.toString());
