e83f6984f8a11f0d16dd8f7d373e5ae7ccfe329d
snailvoyager <kyj88514@gmail.com>
2020-02-26 00:09:54 +0900

    Inserting a Node Into a Sorted Doubly Linked List

---
+++ SW Certi/src/HackerRank/Easy/InsertingDoublyLinkedList.java
@@ -0,0 +1,135 @@
+package HackerRank.Easy;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class InsertingDoublyLinkedList {
+
+	static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the sortedInsert function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data) {
+        DoublyLinkedListNode node = new DoublyLinkedListNode(data);
+        DoublyLinkedListNode curr = head;   //리스트 탐색 노드
+        DoublyLinkedListNode back = head.prev;  //이전 노드 백업
+        
+        if(curr == null)
+            return node;
+        
+        while(curr != null){
+            if(curr.data > node.data)      //리스트 탐색 노드가 더 크면 탐색 종료
+                break;
+            back = curr;
+            curr = curr.next;
+        }
+
+        if(curr != null){
+            if(back != null)           //첫 노드가 아니라면
+                back.next = node;      //왼쪽 노드 연결
+            else
+                head = node;            //첫 노드면 head 변경
+            node.prev = back;
+            node.next = curr;           //오른쪽 노드 연결
+            back = node;
+        } else{                         //curr == null 이면 노드가 리스트 마지막일 때
+            back.next = node;
+            node.prev = curr;
+        }
+        
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            int data = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
