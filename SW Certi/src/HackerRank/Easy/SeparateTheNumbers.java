45a75b5a82ba5a77a9a08877e6125b57126da12e
snailvoyager <kyj88514@gmail.com>
2020-03-04 22:25:36 +0900

    Separate the Numbers 완성

--- SW Certi/src/HackerRank/Easy/SeparateTheNumbers.java
+++ SW Certi/src/HackerRank/Easy/SeparateTheNumbers.java
@@ -5,31 +5,24 @@ import java.util.Scanner;
 public class SeparateTheNumbers {
 
 	static void separateNumbers(String s) {
-        int k = 1;
-        int first = 0 ;
-
-        for(int i=1; i<=s.length()-k; i++, k++){
-            first = Integer.parseInt(s.substring(0, i));
-            
-            int next = Integer.parseInt(s.substring(i, i+k));
-            
-            if(first % 10 == 9)		//9 로 끝나는 정수는 k증가
-            	next = Integer.parseInt(s.substring(i, i+k+1));
-
-            if(next - first == 1){
-                int before = next;
-                int j =0 ;
-                for(j=i+k; j<=s.length()-k; j+=k){
-                    next = Integer.parseInt(s.substring(j, j+k));
-                    if(next - before != 1)
-                        break;
-                    before = next;
-                }
-                if(j == s.length()){
-                    System.out.println("YES " + first);
-                    return;
-                }
-            }
+		if(s.charAt(0) == '0') {
+			System.out.println("NO");
+			return;
+		}
+        for(int i=1; i<=s.length()/2; i++) {
+        	String first = s.substring(0, i);
+        	long start = Long.parseLong(first);		// |s|<=32 !!! Integer.MAX_VALUE = 2147483647
+        	
+        	StringBuilder sb = new StringBuilder();
+        	sb.append(first);
+        	while(sb.length() < s.length()) {
+        		sb.append(++start);
+        	}
+        	
+        	if(sb.toString().equals(s)) {
+        		System.out.println("YES " + first);
+        		return;
+        	}
         }
         System.out.println("NO");
     }
