622aa079f32e254ec048a02a6fd1950150685bdb
snailvoyager <kyj88514@gmail.com>
2020-04-15 23:40:51 +0900

    행렬 생성

---
+++ SW Certi/src/im/Matrix.java
@@ -0,0 +1,22 @@
+package im;
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				System.out.println(i + ", " + j);
+			}
+		}
+		
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				for(int k=0; k<2; k++) {
+					System.out.println(i + ", " + j + ", "+ k);
+				}
+			}
+		}
+	}
+
+}
