0e3982bc77b1c1bed6bfc78726882eca8d2e51ef
snailvoyager <kyj88514@gmail.com>
2020-05-09 20:49:53 +0900

    프로그래머스 H-Index

---
+++ SW Certi/src/programmers/Sorting_Hindex.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Sorting_Hindex {
+	class Solution {
+	    public int solution(int[] citations) {
+	        int answer = 0;
+	        Arrays.sort(citations);
+	        
+	        for(int i=0; i<citations.length; i++){
+	            int h = citations.length - i;
+	            if(citations[i] >= h){
+	                answer = h;
+	                break;
+	            }
+	        }
+	        
+	        return answer;
+	    }
+	}
+}
