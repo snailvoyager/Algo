4d20559d11d798d69e6ea8fccd09147d9d15bc21
snailvoyager <kyj88514@gmail.com>
2020-05-21 23:37:45 +0900

    LeetCode Merge Sorted Array

---
+++ SW Certi/src/LeetCode/Easy/MergeSortedArray.java
@@ -0,0 +1,19 @@
+package LeetCode.Easy;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+	class Solution {
+	    public void merge(int[] nums1, int m, int[] nums2, int n) {
+	        int j = 0;
+	        for(int i=m; i<nums1.length; i++){
+	            if(j == n)
+	                break;
+	            nums1[i] = nums2[j];
+	            j++;
+	        }
+	        
+	        Arrays.sort(nums1);
+	    }
+	}
+}
