4f79f567f6a0e64179ae7ef583415037cf40bcb4
snailvoyager <kyj88514@gmail.com>
2020-05-27 21:54:10 +0900

    LeetCode Number of 1 Bits

---
+++ SW Certi/src/LeetCode/Easy/Numberof1Bits.java
@@ -0,0 +1,15 @@
+package LeetCode.Easy;
+
+public class Numberof1Bits {
+	public class Solution {
+	    // you need to treat n as an unsigned value
+	    public int hammingWeight(int n) {
+	        int result = 0;
+	        for(int i=0; i<32; i++){
+	            if((n & (1<<i)) != 0)
+	                result++;
+	        }
+	        return result;
+	    }
+	}
+}
