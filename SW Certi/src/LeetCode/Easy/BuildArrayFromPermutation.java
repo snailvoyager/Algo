6b2172b10da3da0929be50b9a180ea78d7a54a34
snailvoyager <kyj88514@gmail.com>
2021-07-29 00:03:03 +0900

    LeetCode No.1920

---
+++ SW Certi/src/LeetCode/Easy/BuildArrayFromPermutation.java
@@ -0,0 +1,15 @@
+package LeetCode.Easy;
+
+public class BuildArrayFromPermutation {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int[] ans = new int[nums.length];
+
+            for(int i=0; i<nums.length; i++) {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+    }
+}
