f14d3b9a77534b3c32fedef57e55d04b8e41a26c
snailvoyager <kyj88514@gmail.com>
2022-07-28 23:17:28 +0900

    LeetCode No.721 using Union-Find

---
+++ SW Certi/src/LeetCode/Medium/AccountsMerge.java
@@ -0,0 +1,67 @@
+package LeetCode.Medium;
+
+import java.util.*;
+
+public class AccountsMerge {
+    int[] parent;
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        parent = new int[accounts.size()];
+        for (int i=0; i<parent.length; i++) {
+            parent[i] = i;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i=0; i<accounts.size(); i++) {
+            List<String> emailList = accounts.get(i);
+
+            for (int j=1; j<emailList.size(); j++) {
+                String s = emailList.get(j);
+
+                if (map.containsKey(s)) {
+                    int k = map.get(s);
+                    union(k, i, accounts);
+                } else {
+                    map.put(s, i);
+                }
+            }
+        }
+
+        List<List<String>> result = new LinkedList<>();
+        for (List<String> account : accounts) {
+            if (account.size() > 0) {
+                String name = account.get(0);
+                account.remove(0);
+
+                Set<String> set = new HashSet<>(account);
+                account.clear();
+                account.addAll(set);
+                Collections.sort(account);
+                account.add(0, name);
+
+                result.add(account);
+            }
+        }
+        return result;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int x, int y, List<List<String>> accounts) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY)
+            return;
+
+        parent[rootX] = rootY;
+        accounts.get(rootX).remove(0);      //remove name;
+        accounts.get(rootY).addAll(accounts.get(rootX));
+        accounts.get(rootX).clear();
+    }
+}
