abb1e94ff3e830365b01e3c61ee339c0e7de52d6
snailvoyager <kyj88514@gmail.com>
2022-08-12 00:54:08 +0900

    Time: 0 ms (100.00%), Space: 42.7 MB (38.58%) - LeetHub

---
+++ SW Certi/src/LeetCode/75-sort-colors/75-sort-colors.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        //selectionSort(nums);
+        int first = 0;
+        int last = nums.length-1;
+        int i = 0;
+        
+        while (i <= last) {
+            if (nums[i] == 0) {
+                nums[i] = nums[first];
+                nums[first] = 0;
+                first++;
+                i++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[last];
+                nums[last] = 2;
+                last--;
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    public void selectionSort(int[] nums) {
+        for (int i=0; i<nums.length-1; i++) {
+            int minIdx = i;
+            for (int j=i+1; j<nums.length; j++) {
+                //최소값 갱신
+                if (nums[minIdx] > nums[j]) {
+                    minIdx = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
+        }
+    }
+}
